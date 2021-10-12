feecb862b5acec0c7020980c72ef26a698743e26
ll3097 <47397212+ll3097@users.noreply.github.com>
2021-10-11 23:05:26 -0400

    Add files via upload

---
+++ Temp figures along edges/plotTempABCD.m
@@ -0,0 +1,49 @@
+% 1.plot the temperature variation along edge ABCD of the coarse mesh
+node=importdata('nodeinfo1.txt');
+load('dcoarse.mat');
+aculength=zeros(1,28); sumlengthAD=0;
+Xad=zeros(1,29);Yad=zeros(1,29);length=zeros(1,28);
+Nad=[4,1,66:1:81,6,16:1:24,7];
+Xad=node(Nad',2);Yad=node(Nad',3);
+for i=1:1:28
+x1=Xad(i);y1=Yad(i);
+x2=Xad(i+1);y2=Yad(i+1);
+length(1,i)=sqrt((x2-x1)^2+(y2-y1)^2);
+aculength(1,i)=sum(length(1:i))
+sumlengthAD=sumlengthAD+length(1,i);
+end
+aculength=[0,aculength];
+r=0.1:1.15/28:1.25
+Tad=d(Nad');
+plot(r,Tad,'k*-','LineWidth',1);
+xlabel('radius R');
+ylabel('Temperature variation (Celsius)')
+title('Temperature variation along the edge ABCD')
+legend('coarse mesh')
+grid on
+hold on
+
+% 2.plot the temperature variation along edge ABCD of the refined mesh
+load('drefined.mat');
+node=importdata('nodeinfo2.txt');
+aculength=zeros(1,58); sumlengthAD=0;
+Xad=zeros(1,59);Yad=zeros(1,59);length=zeros(1,58);
+Nad=[4,20,21,1,131:164,6,26:44,7];
+Xad=node(Nad',2);Yad=node(Nad',3);
+for i=1:1:58
+x1=Xad(i);y1=Yad(i);
+x2=Xad(i+1);y2=Yad(i+1);
+length(1,i)=sqrt((x2-x1)^2+(y2-y1)^2);
+aculength(1,i)=sum(length(1:i))
+sumlengthAD=sumlengthAD+length(1,i);
+end
+aculength=[0,aculength];
+r=0.1:1.15/58:1.25
+Tad=d(Nad');
+plot(r,Tad,'bo-','LineWidth',1);
+xlabel('radius R');
+ylabel('Temperature variation (Celsius)')
+title('Temperature variation along the edge ABCD')
+legend('coarse mesh','refined mesh')
+grid on
+hold off
---
+++ Temp figures along edges/plotTempCFG.m
@@ -0,0 +1,46 @@
+% plot the temperature variation along edge CFG
+node=importdata('nodeinfo1.txt');
+load('dcoarse.mat');
+aculength=zeros(1,18); sumlengthAD=0;
+Xcg=zeros(1,19);Ycg=zeros(1,19);length=zeros(1,18);
+Nad=[6,5,14,5,36:49,9];
+Xcg=node(Nad',2);Ycg=node(Nad',3);
+for i=1:1:18
+x1=Xcg(i);y1=Ycg(i);
+x2=Xcg(i+1);y2=Ycg(i+1);
+length(1,i)=sqrt((x2-x1)^2+(y2-y1)^2);
+aculength(1,i)=sum(length(1:i))
+sumlengthAD=sumlengthAD+length(1,i);
+end
+Csita=Ycg(19)/sqrt(Ycg(19)^2+Xcg(19)^2);
+sita=(acos(Csita)/pi)*180; % sita is the angle between vector node#5 point to point(0,0) and y axial.
+SITA=45:45/18:90;
+Tad=d(Nad');
+plot(SITA,Tad,'k*-','LineWidth',2);
+hold on
+
+node=importdata('nodeinfo2.txt');
+load('drefined.mat');
+aculength=zeros(1,35); sumlengthAD=0;
+Xcg=zeros(1,36);Ycg=zeros(1,36);length=zeros(1,35);
+Nad=[6,25:-1:22,5,68:1:96,9];
+Xcg=node(Nad',2);Ycg=node(Nad',3);
+for i=1:1:35
+x1=Xcg(i);y1=Ycg(i);
+x2=Xcg(i+1);y2=Ycg(i+1);
+length(1,i)=sqrt((x2-x1)^2+(y2-y1)^2);
+aculength(1,i)=sum(length(1:i))
+sumlengthAD=sumlengthAD+length(1,i);
+end
+Csita=Ycg(36)/sqrt(Ycg(36)^2+Xcg(36)^2);
+sita=(acos(Csita)/pi)*180; % sita is the angle between vector node#5 point to point(0,0) and y axial.
+SITA=45:45/35:90;
+aculength=[0,aculength];
+Tad=d(Nad');
+plot(SITA,Tad,'bo-','LineWidth',2);
+xlabel('the angle between the vector of each node coordinate and x axial in degree ');
+ylabel('Temperature variation (Celsius)');
+title('Temperature variation along the edge CFG');
+grid on
+legend('coarse mesh','refined mesh');
+hold off
